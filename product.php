f9d121ed320d3ffc6768b3acd62b5645dc43cb09
stecieb <90315208+stecieb@users.noreply.github.com>
2021-11-15 10:45:23 +0100

    Delete product.php

--- product.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/twig.php';
-$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
-echo $twig ->render('product.html.twig', ['product' => $products]);
