834378c1029511caccc522f42bc395eaca5c15a5
stecieb <90315208+stecieb@users.noreply.github.com>
2021-11-15 10:43:57 +0100

    Add files via upload

---
+++ product.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ . '/../config/twig.php';
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+echo $twig ->render('product.html.twig', ['product' => $products]);
